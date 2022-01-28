641c990a5e695eb7a39c15f90531599fadfad959
yzh <1255245824@qq.com>
2022-01-28 13:33:11 +0800

    bot实例

---
+++ bot.js
@@ -0,0 +1,30 @@
+/**
+ * @author Hilbert Yi
+ * @time 2022-01-11
+ * @digest 入口文件
+ */
+const { Wechaty } = require("wechaty");
+const {PuppetPadlocal} = require("wechaty-puppet-padlocal");
+const config = require("./config");
+
+const token = config.PUPPET_TOKEN;
+const botName = config.BOTNAME;
+
+// 创建机器人
+const bot = new Wechaty({
+  puppet: new PuppetPadlocal({
+    token
+  }),
+  name: botName
+})
+
+module.exports = bot;
+
+//web协议
+// const bot = new Wechaty({
+//   name: "WeChat-Robot"
+// });
+
+
+
+
