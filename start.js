810a741e66b215d5520edc505aa26472db543120
yzh <1255245824@qq.com>
2022-02-16 21:54:47 +0800

    :bulb:佛祖之力加持

--- start.js
+++ start.js
@@ -1,6 +1,38 @@
-const login = require("./listeners/on-login");
-const message = require("./listeners/on-message");
-const scan = require("./listeners/on-scan");
+/*
+ * @Author: Yi Zhihang
+ * @Create: 2022-01-28 11:39:08
+ * @LastEditTime: 2022-02-02 21:30:13
+ * @Description: 入口文件，启动机器人实例
+ */
+/*
+ *                        _oo0oo_
+ *                       o8888888o
+ *                       88" . "88
+ *                       (| -_- |)
+ *                       0\  =  /0
+ *                     ___/`---'\___
+ *                   .' \\|     |// '.
+ *                  / \\|||  :  |||// \
+ *                 / _||||| -:- |||||- \
+ *                |   | \\\  - /// |   |
+ *                | \_|  ''\---/''  |_/ |
+ *                \  .-\__  '-'  ___/-. /
+ *              ___'. .'  /--.--\  `. .'___
+ *           ."" '<  `.___\_<|>_/___.' >' "".
+ *          | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+ *          \  \ `_.   \_ __\ /__ _/   .-` /  /
+ *      =====`-.____`.___ \_____/___.-`___.-'=====
+ *                        `=---='
+ * 
+ * 
+ *      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * 
+ *            佛祖保佑     永不宕机     永无BUG
+ */
+
+const login = require("./listeners/on-login"); 
+const message = require("./listeners/on-message"); 
+const scan = require("./listeners/on-scan"); 
 const friendship = require("./listeners/on-friendship");
 const roomJoin = require("./listeners/on-room-join");
 const roomLeave = require("./listeners/on-room-leave");
