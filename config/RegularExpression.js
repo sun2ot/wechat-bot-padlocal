38cf6b222dbdd7371cd410b7748c4db05015f351
yzh <1255245824@qq.com>
2022-02-24 16:01:24 +0800

    :construction:优化代码格式

---
+++ config/RegularExpression.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: Yi Zhihang
+ * @Create: 2022-02-24 15:22:22
+ * @LastEditTime: 2022-02-24 15:25:10
+ * @Description: 指令正则表达式
+ */
+module.exports = {
+    IGNORE: /^屏蔽/,
+    UN_IGNORE: /^解除屏蔽/
+}
