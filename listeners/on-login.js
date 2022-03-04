d4cf7850ec223c8e97de60207b9a6306c5f2b0ca
Hilbert Yi <1255245824@qq.com>
2022-03-04 16:34:45 +0800

    :sparkles:新增休息提醒功能

--- listeners/on-login.js
+++ listeners/on-login.js
@@ -17,14 +17,15 @@ async function onLogin(user) {
   console.log(`比庆元哥哥差一点点的${user}登录了`);
   //创建定时发送群消息任务
   await rolling();
+  await rest();
 }
 
 /**
- * @func 10:25定时给指定群发送消息
+ * @func 定时群消息
  */
 async function rolling() {
   schedule.setSchedule(
-    moment().format("X"),
+    'group',
     {
       hour: 9,
       minute: 00,
@@ -33,7 +34,7 @@ async function rolling() {
     async () => {
       const today = moment().format("MM月DD日"); //日期
       const poison = await request.getSoup(); //毒鸡汤
-      const str = `今天是${today},你毕业设计做完了吗?\n${poison}`;
+      const str = `\n今天是${today},你毕业设计做完了吗?\n---------------\n${poison}`;
       for (let i=0; i<config.WEBROOM.length; i++) {
         const room = await bot.Room.find({
           topic: config.WEBROOM[i],
@@ -43,6 +44,35 @@ async function rolling() {
       }
     }
   );
+}
+
+/**
+ * @func 久坐提醒(30min/次)
+ */
+async function rest() {
+  schedule.setSchedule(
+    {hour: 12, minute: 30},
+    () => {
+      console.log('久坐提醒已上线');
+      schedule.setSchedule(
+        'start',
+        '*/30 * * * *',
+        async () => {
+          console.log('time for rest');
+          const master = await bot.Contact.find({ alias: config.MYSELF });
+          master.say(`工作30min了，让眼睛休息下吧！`);
+        }
+      )
+    }
+  )
+
+  schedule.setSchedule(
+    {hour: 19}, //晚上7点之后就没太阳了
+    () => {
+      const success = schedule.cancelJobName('start');
+      console.log(success === true ? '久坐提醒已关闭' : '久坐提醒已失败');
+    }
+  )
 }
 
 module.exports = onLogin;
