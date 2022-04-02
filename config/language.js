c81ba5981e8423ad803f06159ccaf4f6717a6dbf
Hilbert Yi <1255245824@qq.com>
2022-04-02 16:17:08 +0800

    翻译功能可选语言

---
+++ config/language.js
@@ -0,0 +1,24 @@
+/*
+ * @Author: Yi Zhihang
+ * @Create: 2022-04-02 13:26:11
+ * @LastEditTime: 2022-04-02 13:54:56
+ * @Description: 翻译功能可选语言
+ */
+const from = {
+    /**
+     * 源语言支持auto
+     */
+    Auto: 'auto',
+    English: 'en',
+    Chinese: 'zh'
+}
+
+const to = {
+    English: 'en',
+    Chinese: 'zh',
+    Japanese: 'jp'
+}
+
+module.exports = {
+    from, to
+}
